20fe92d5797314adbcbbe1c41c64f28206053d0d
Vladimir <v1ztep@gmail.com>
2021-06-10 23:11:57 +0300

    Подключил DialogFlow к боту

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 import telegram
 from dotenv import load_dotenv
+from google.cloud import dialogflow
 from telegram.ext import CommandHandler
 from telegram.ext import Filters
 from telegram.ext import MessageHandler
@@ -22,12 +23,30 @@ class TelegramLogsHandler(logging.Handler):
         self.bot.send_message(chat_id=self.tg_chat_id, text=log_entry)
 
 
+def detect_intent_text(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    return response.query_result
+
+
 def start(bot, update):
-    update.message.reply_text('Здравствуйте')
+    update.message.reply_text('Чатбот активирован!')
 
 
-def echo(bot, update):
-    update.message.reply_text(update.message.text)
+def reply(bot, update):
+    project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
+    session_id = update.message.chat.id
+    language_code = 'ru-RU'
+    text = update.message.text
+
+    intent = detect_intent_text(project_id, session_id, text, language_code)
+    update.message.reply_text(intent.fulfillment_text)
 
 
 def error(bot, update, error):
@@ -44,10 +63,9 @@ def main():
     logger.info('Бот запущен')
 
     updater = Updater(tg_token)
-
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.text, reply))
     dp.add_error_handler(error)
     updater.start_polling()
     updater.idle()
